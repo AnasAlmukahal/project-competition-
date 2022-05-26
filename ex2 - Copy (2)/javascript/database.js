5fa544f00a493795553636ede998ec8849f1c3ce
AnasAlmukahal <70823788+AnasAlmukahal@users.noreply.github.com>
2022-05-26 17:59:34 +0300

    Add files via upload

---
+++ ex2 - Copy (2)/javascript/database.js
@@ -0,0 +1,129 @@
+const products = [
+    {
+      id: 0,
+      name: "razer blade 15",
+      price: 100,
+      instock: 100,
+      description:
+        "laptop for example",
+      imgSrc: "pictures/laptop.jpg",
+    },
+    {
+      id: 1,
+      name: "hp 290 microtower desktop",
+      price: 123,
+      instock: 43,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "pictures/computer1.jpg",
+    },
+    {
+      id: 2,
+      name: "literally best computer",
+      price: 700,
+      instock: 10,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "pictures/computer1.jpg",
+    },
+    {
+      id: 3,
+      name: "Typical jordan computer",
+      price: 123,
+      instock: 5,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "pictures/computer1.jpg",
+    },
+    {
+      id: 4,
+      name: "king's computer",
+      price: 29.99,
+      instock: 4,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "pictures/computer1.jpg",
+    },
+    {
+      id: 5,
+      name: "lenovo usb 3-in1 hub",
+      price: 13,
+      instock: 40,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+      imgSrc: "pictures/usb-hub.jpg",
+    },
+    {
+        id: 6,
+        name: "omni-food headphones with mic",
+        price: 21,
+        instock: 40,
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+        imgSrc: "pictures/headphones.jpg",
+      },
+      {
+        id: 7,
+        name: "razer mouse",
+        price: 7,
+        instock: 40,
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+        imgSrc: "pictures/mouse-normal.jpg",
+      },
+      {
+        id: 8,
+        name: "crosshair microphone",
+        price: 35,
+        instock: 40,
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+        imgSrc: "pictures/microphone.jpg",
+      },
+      {
+        id: 9,
+        name: "shrek 2 for ps2",
+        price: 100,
+        instock: 40,
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+        imgSrc: "pictures/shrek.jpg",
+      },
+      {
+        id: 10,
+        name: "Intel i9-9900KS Processor",
+        price: 210,
+        instock: 40,
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+        imgSrc: "pictures/intel.jpg",
+      },
+      {
+        id: 11,
+        name: "AMD Ryzen™ 7 5800X3D",
+        price: 300,
+        instock: 40,
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+        imgSrc: "pictures/ryzen.jpg",
+      },
+      {
+        id: 12,
+        name: "razer mouse",
+        price: 40,
+        instock: 40,
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+        imgSrc: "pictures/gamingMouse.jpg",
+      },
+      {
+        id: 13,
+        name: "mechanical keyboard",
+        price: 35,
+        instock: 40,
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+        imgSrc: "pictures/keyboard.jpg",
+      },
+  ];
+  
